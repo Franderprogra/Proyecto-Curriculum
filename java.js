350721b73a9cf565d4d4b46acfd16df2395b0b77
Franderprogra <franderprogra@gmail.com>
2024-04-29 15:44:33 -0600

    segundo dia trabajado

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
-let button = document.getElementById(button1) 
-console.log.apply(click)
-
-botton.addEventListener("click" function () {
-    button.innerHTML=
+let button2= document.getElementById("button1")
+button2.addEventListener ("click", function ()  {
+    alert ("Hola! Soy Frander, me destaco por ser muy eficienten mi trabajo ademas de ser responsable, cuando trabajo me enfoco en hacer las cosasde la mejor manera y de forma rapida sin perder la calidad de mi trabajo, ademas de que creoque puedo apotar mucho conocimiento y ideas a su empresa ")
+    console.log(hola)
+    
 })
